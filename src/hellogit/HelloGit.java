e7954ea273ad32c4337409942bc4e940a09e9f77
LC01 <LC01@LC01-PC>
2018-01-12 11:20:54 +0800

    Edit file hello git

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -16,7 +16,18 @@ public class HelloGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        showtext();
+        showbye();
+    }
+    
+    public static void showtext () {
         System.out.println("Hello Git");
+        System.out.println("Are You Ready ?");
+    }
+    
+    public static void showbye () {
+        System.out.println("Hello Git");
+        System.out.println("good bye");
     }
     
 }
